9b29b959a89fa008e9363c616ef7821968327bab
Administrator <Administrator@USER-20180827VT>
2019-12-11 15:05:10 +0800

    first update

--- src/com/jhk/test/HelloWorld.java
+++ src/com/jhk/test/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jhk.test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hajimede");
+		System.out.println("hajimede"+"update in eclipse");
 	}
 }
